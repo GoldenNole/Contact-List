4c1d21a0d42597d1a5bea06dbf9a6325c097b67f
GoldenNole <prstokes37@gmail.com>
2023-06-22 15:23:11 -0500

    added stuff

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -6,7 +6,29 @@ const API_URL = "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users";
 
 
 export default function SelectedContact({selectedContactId, setSelectedContactId}) {
-    const dummyContact = { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" };
+    const dummyContact = {
+  "id": 1,
+  "name": "Mrs. Jerald Schulist",
+  "username": "Antwan",
+  "email": "Coby_Zieme@libby.tv",
+  "address": {
+    "street": "Morissette Heights",
+    "suite": "Apt. 633",
+    "city": "Port Liashire",
+    "zipcode": "04270",
+    "geo": {
+      "lat": "27.7501",
+      "lng": "-83.1776"
+    }
+  },
+  "phone": "(663)839-3814 x845",
+  "website": "arvel.io",
+  "company": {
+    "name": "D'Amore-Krajcik",
+    "catchPhrase": "Down-sized empowering Graphic Interface",
+    "bs": "extend cross-media relationships"
+  }
+};
 
     const [contact, setContact] = useState(dummyContact);
     useEffect(() => {
@@ -26,10 +48,12 @@ export default function SelectedContact({selectedContactId, setSelectedContactId
     console.log("Contact: ", contact);
     return (
         <div>
-            <button onClick={() => setSelectedContactId(null)}>Back to Contacts</button>
             <h1>{contact.name}</h1>
             <h2>{contact.email}</h2>
             <h2>{contact.phone}</h2>
+            <h2>{contact.website}</h2>
+            <h2>{contact.company.name}</h2>
+            <button onClick={() => setSelectedContactId(null)}>Back to Contacts</button>
         </div>
     );
 }
